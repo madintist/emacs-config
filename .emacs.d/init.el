76a1bd2e4a3e24f75dbe2575fd87ea79ee45144f
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-09-04 08:44:32 -0500

    Load load-packages.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,5 +7,8 @@
 ; Load the `load-directory` function
 (load "~/.emacs.d/load-directory")
 
+; Load the packages system (we need this before config)
+(load "~/.emacs.d/load-packages")
+
 ; Load the config directory
 (load-directory "~/.emacs.d/config")
