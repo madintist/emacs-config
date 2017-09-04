bc34791c09408d16f27f79aac972119e0216bcbf
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-09-04 08:30:42 -0500

    Added code to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,11 @@
 ;;; init.el - Load my Emacs configuration
+
+
+
+;;; Code:
+
+; Load the `load-directory` function
+(load "~/.emacs.d/load-directory")
+
+; Load the config directory
+(load-directory "~/.emacs.d/config")
