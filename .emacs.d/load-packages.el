be5fe611f7f22b37446ec2faee70cf158b514eb6
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-09-04 08:34:03 -0500

    Added Commentary section to load-packages.el

---
+++ .emacs.d/load-packages.el
@@ -0,0 +1,9 @@
+;;; load-packages.el --- Load package system(s) into Emacs
+
+
+
+;;; Commentary:
+;; This file will load the necessary package archives
+;; (GNU, MELPA, Marmalade, ...) into Emacs.
+;; It also checks that all of our packages are installed
+;; and updates them as necessary.
