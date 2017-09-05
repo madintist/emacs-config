868616eaeab303fa98641d8006cf6822dc603010
Joshua <josh@luckyorange.com>
2017-09-05 12:19:28 -0500

    Added mocha-snippets

---
+++ .emacs.d/config/packages/mocha-snippets.el
@@ -0,0 +1,18 @@
+;;; mocha-snippets.el --- Configuration for mocha-snippets
+
+
+
+;;; Commentary:
+;; Add mocha snippets to YASnippets
+
+
+
+;;; Code:
+(use-package mocha-snippets
+  :ensure t
+  :init
+  (setq mocha-snippets-add-space-after-function-keyword t))
+
+
+
+;;; mocha-snippets.el ends here
