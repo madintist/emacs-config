89d3c7bb78c63080be270024092babf3ff32dac3
Joshua <josh@luckyorange.com>
2017-09-06 08:12:23 -0500

    Keep show-paren-mode on

---
+++ .emacs.d/config/packages/show-paren.el
@@ -0,0 +1,15 @@
+;;; show-paren.el --- Configuration for show-paren-mode
+
+
+
+;;; Commentary:
+;; Turn on show-paren-mode by default
+
+
+
+;;; Code:
+(show-paren-mode t)
+
+
+
+;;; show-paren.el ends here
