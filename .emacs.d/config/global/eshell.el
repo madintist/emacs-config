9d488b4036aafb1bad13f773be34aa6ed6b631bf
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-09-09 16:28:31 -0500

    Configure Eshell and smart display

--- .emacs.d/config/global/eshell.el
+++ .emacs.d/config/global/eshell.el
@@ -3,10 +3,20 @@
 
 
 ;;; Commentary:
+;; Load and configure Eshell with smart mode
 
 
 
 ;;; Code:
+
+;; Load Eshell and Smart mode
+(require 'eshell)
+(require 'em-smart)
+
+;; Settings
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end t)
 
 
 
