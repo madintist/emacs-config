7f95cfa91dac579a734e16e8ca9b33005bf86666
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-09-09 16:38:32 -0500

    Added eshell-git-prompt

---
+++ .emacs.d/config/packages/eshell-git-prompt.el
@@ -0,0 +1,18 @@
+;;; eshell-git-prompt.el --- Configuration for eshell-git-prompt
+
+
+
+;;; Commentary:
+;; Add a nice prompt to Eshell
+
+
+
+;;; Code:
+(use-package eshell-git-prompt
+  :ensure t
+  :config
+  (eshell-git-prompt-use-theme 'powerline))
+
+
+
+;;; eshell-git-prompt.el ends here
