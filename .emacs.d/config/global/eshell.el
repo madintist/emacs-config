3ce703ef19fcba598ea3f38f296bfb22e7a4c295
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-09-09 16:35:48 -0500

    Revert "Use use-package"
    This reverts commit d912d0289a303ffb96a700e300947a564bafbcbc.

--- .emacs.d/config/global/eshell.el
+++ .emacs.d/config/global/eshell.el
@@ -8,12 +8,16 @@
 
 
 ;;; Code:
-(use-package em-smart
-  :ensure t
-  :init
-  (setq eshell-where-to-jump 'begin)
-  (setq eshell-review-quick-commands nil)
-  (setq eshell-smart-space-goes-to-end t))
+
+;; Load Eshell and Smart mode
+(require 'eshell)
+(require 'em-smart)
+
+;; Settings
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end t)
+
 
 
 ;;; eshell.el ends here
