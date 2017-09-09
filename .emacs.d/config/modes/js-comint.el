1db1e220f53883dc19c48f6d4e765b72a059ba7d
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-09-09 18:33:02 -0500

    Added js-comint

---
+++ .emacs.d/config/modes/js-comint.el
@@ -0,0 +1,23 @@
+;;; js-comint.el --- Configuration for js-comint
+
+
+
+;;; Commentary:
+
+
+
+;;; Code:
+(use-package js-comint
+  :ensure t
+  :init
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
+              (local-set-key (kbd "C-M-x") 'js-send-last-sexp-and-go)
+              (local-set-key (kbd "C-c b") 'js-send-buffer)
+              (local-set-key (kbd "C-c C-b") 'js-send-buffer-and-go)
+              (local-set-key (kbd "C-c l") 'js-load-file-and-go))))
+
+
+
+;;; js-comint.el ends here
