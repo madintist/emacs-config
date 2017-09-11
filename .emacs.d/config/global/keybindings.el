4ac115a921db23eb5393df77e8c677b9311b6116
Joshua <josh@luckyorange.com>
2017-09-11 07:19:23 -0500

    Added keybindings for ANSI-Term

--- .emacs.d/config/global/keybindings.el
+++ .emacs.d/config/global/keybindings.el
@@ -10,7 +10,10 @@
 ;;; Code:
 
 ;; Terminal Commands:
-(global-set-key (kbd "C-x t") 'eshell) ; Start Eshell
+(global-set-key (kbd "C-x t b") (lambda () (interactive) (ansi-term "/bin/bash" "ANSI-Term : Bash"))) ; Start ANSI-Term with a Bash shell
+(global-set-key (kbd "C-x t e") 'eshell) ; Start Eshell
+(global-set-key (kbd "C-x t z") (lambda () (interactive) (ansi-term "/bin/zsh" "ANSI-Term : Zsh"))) ; Start ANSI-Term with a Zsh shell
+
 
 ;; Window Commands:
 (global-set-key (kbd "C-x T F") 'toggle-frame-fullscreen) ; Set keybinding for fullscreen mode
