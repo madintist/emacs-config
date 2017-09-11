20f4070f2c4de124c0611ab16cd6bc0cacdd34cc
Joshua <josh@luckyorange.com>
2017-09-11 11:13:27 -0500

    Added Node.js REPL keybinding

--- .emacs.d/config/global/keybindings.el
+++ .emacs.d/config/global/keybindings.el
@@ -12,6 +12,7 @@
 ;; Terminal Commands:
 (global-set-key (kbd "C-x t b") (lambda () (interactive) (ansi-term "/bin/bash" "ANSI-Term : Bash"))) ; Start ANSI-Term with a Bash shell
 (global-set-key (kbd "C-x t e") 'eshell) ; Start Eshell
+(global-set-key (kbd "C-x t j") 'run-js) ; Start Node.js REPL
 (global-set-key (kbd "C-x t z") (lambda () (interactive) (ansi-term "/bin/zsh" "ANSI-Term : Zsh"))) ; Start ANSI-Term with a Zsh shell
 
 
