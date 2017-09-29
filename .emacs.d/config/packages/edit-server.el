a5a0c78d5f8fd42b51b512f98f9ea825a80b1202
Joshua Kleveter <josh@luckyorange.com>
2017-09-29 10:12:15 -0500

    Added edit-server

---
+++ .emacs.d/config/packages/edit-server.el
@@ -0,0 +1,18 @@
+;;; edit-server.el --- Configuration for edit-server
+
+
+
+;;; Commentary:
+;; This is supposed to start an edit server so we can edit stuff from Chrome...
+
+
+
+;;; Code:
+(use-package edit-server
+  :ensure t
+  :config
+  (edit-server-start))
+
+
+
+;;; edit-server.el ends here
