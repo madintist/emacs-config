de1c3e3a961e1f40ac0c0149818e9994eaf3d6cc
Joshua Kleveter <josh@luckyorange.com>
2017-10-19 08:42:25 -0500

    Updated snippets

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -55,7 +55,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tango-dark)))
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (vue-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
