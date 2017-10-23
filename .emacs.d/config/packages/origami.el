80988f04a48a22ad8bfad7a0f012f7c37b4bdbc5
Joshua Kleveter <josh@luckyorange.com>
2017-10-23 09:51:29 -0500

    Added origami.el

---
+++ .emacs.d/config/packages/origami.el
@@ -0,0 +1,17 @@
+;;; origami.el --- Configuration for origami mode
+
+
+
+;;; Commentary:
+
+
+
+;;; Code:
+(use-package origami
+  :ensure t
+  :config
+  (global-origami-mode t))
+
+
+
+;;; origami.el ends here
