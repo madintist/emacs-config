7f13947886a9295a0be9c9131b0235386319ddad
Joshua Kleveter <josh@luckyorange.com>
2017-10-23 09:40:54 -0500

    Customized how Projectile finds a project

--- .emacs.d/config/packages/projectile.el
+++ .emacs.d/config/packages/projectile.el
@@ -10,6 +10,10 @@
 ;;; Code:
 (use-package projectile
   :ensure t
+  :init
+  (setq projectile-project-root-files-functions '(projectile-root-top-down))
+  (setq projectile-project-root-files
+        '(".git" ".bzr" ".svn" ".hg"  "_darcs" ".projectile" "Makefile"))
   :config
   (projectile-mode t))
 
