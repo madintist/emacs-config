c8eabd91418e005baad3d16a7b047516ffea5d4b
Joshua Kleveter <josh@luckyorange.com>
2017-10-27 16:49:38 -0500

    Added additional mode hooks

--- .emacs.d/config/packages/lsp-javascript.el
+++ .emacs.d/config/packages/lsp-javascript.el
@@ -11,7 +11,10 @@
 (use-package lsp-javascript-typescript
   :ensure t
   :init
-  (add-hook 'js2-mode-hook #'lsp-javascript-typescript-enable))
+  (add-hook 'js2-mode-hook #'lsp-javascript-typescript-enable)
+  (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
+  (add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable)
+  (add-hook 'rjsx-mode-hook #'lsp-javascript-typescript-enable))
 
 
 ;;; lsp-javascript.el ends here
