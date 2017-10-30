bc53a62f55d56033f961779da30547b305f9ec1f
Joshua Kleveter <josh@luckyorange.com>
2017-10-30 16:06:29 -0500

    Remove binding for '@' symbol in jsdoc comments

--- .emacs.d/config/packages/js-doc.el
+++ .emacs.d/config/packages/js-doc.el
@@ -15,8 +15,7 @@
   (add-hook 'js2-mode-hook
             #'(lambda ()
                 (define-key js2-mode-map "\C-ci" 'js-doc-insert-function-doc)
-                (define-key js2-mode-map "\C-cf" 'js-doc-insert-file-doc)
-                (define-key js2-mode-map "@" 'js-doc-insert-tag))))
+                (define-key js2-mode-map "\C-cf" 'js-doc-insert-file-doc))))
 
 
 
