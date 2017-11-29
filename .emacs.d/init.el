459f849ebeb69365c7e84a1d7d7edc569c84362b
Joshua Kleveter <josh@luckyorange.com>
2017-11-29 07:55:04 -0600

    Removed vue-mode from custom

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -56,8 +56,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
- '(package-selected-packages (quote (vue-mode use-package))))
+ '(custom-enabled-themes (quote (tango-dark))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
