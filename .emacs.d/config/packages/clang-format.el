8f78ded31319ff86bb4a9bfd14ea08445690574d
Joshua Kleveter <joshua.kleveter@gmail.com>
2017-12-29 08:02:04 -0600

    Added clang-format.el file

---
+++ .emacs.d/config/packages/clang-format.el
@@ -0,0 +1,15 @@
+;;; clang-format.el --- Formatting for C languages
+
+
+
+;;; Commentary:
+;; Add clang-format.el package
+;; Configure keybindings for package
+
+
+
+;;; Code:
+
+
+
+;;; clang-format.el ends here
