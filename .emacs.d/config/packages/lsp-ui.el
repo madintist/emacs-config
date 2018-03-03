d7d5d90b8f49d7b7c76ee25ae61cceed7a8a6bb5
Joshua Kleveter <joshua.kleveter@gmail.com>
2018-03-03 12:53:21 -0600

    Remap xref-find-definitions/references to lsp-ui-peek

--- .emacs.d/config/packages/lsp-ui.el
+++ .emacs.d/config/packages/lsp-ui.el
@@ -10,6 +10,9 @@
 ;;; Code:
 (use-package lsp-ui
   :ensure t
+  :bind
+  (([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+   ([remap xref-find-references] . lsp-ui-peek-find-references))
   :init
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
