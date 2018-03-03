699551c7ec0f00e1c0d7295aa64b059a834d598d
Joshua Kleveter <joshua.kleveter@gmail.com>
2018-03-03 12:22:40 -0600

    Added lsp-ui.el

---
+++ .emacs.d/config/packages/lsp-ui.el
@@ -0,0 +1,15 @@
+;;; lsp-ui.el --- UI components for lsp-mode.el
+
+
+
+;;; Commentary:
+;; Load this when we load lsp-mode
+
+
+
+;;; Code:
+(use-package lsp-ui
+  :ensure t
+  :hook lsp-mode)
+
+;;; lsp-ui.el ends here
