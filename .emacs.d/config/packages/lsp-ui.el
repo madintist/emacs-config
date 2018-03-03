3b609119139e2f3f0f8257e60eaa7a1e23deac34
Joshua Kleveter <joshua.kleveter@gmail.com>
2018-03-03 12:56:36 -0600

    Use normal keybinding syntax

--- .emacs.d/config/packages/lsp-ui.el
+++ .emacs.d/config/packages/lsp-ui.el
@@ -11,8 +11,8 @@
 (use-package lsp-ui
   :ensure t
   :bind
-  (([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-   ([remap xref-find-references] . lsp-ui-peek-find-references))
+  (("M-." . lsp-ui-peek-find-definitions)
+   ("M-?" . lsp-ui-peek-find-references))
   :init
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
