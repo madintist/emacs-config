28362e4e677437d99a192e63305dc12ee428f947
Joshua Kleveter <joshua.kleveter@gmail.com>
2018-03-23 07:48:28 -0500

    Use enh-ruby-mode for *.rb files

---
+++ .emacs.d/config/modes/enhanced-ruby-mode.el
@@ -0,0 +1,18 @@
+;; enhanced-ruby-mode.el --- Configuration for enhanced-ruby-mode
+
+
+
+;;; Commentary:
+;; Load and set up enhanced-ruby-mode
+;; - Use enhanced-ruby-mode for *.rb files
+
+
+
+;;; Code:
+(use-package enh-ruby-mode
+  :ensure t
+  :mode ("\\.rb\\'" . enh-ruby-mode))
+
+
+
+;;; enhanced-ruby-mode.el ends here
